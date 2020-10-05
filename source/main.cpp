e81336760bfcdb927e0464aeb23bed70f85e2ecb
Stas_SD <sidelnikov.si@phystech.edu>
2020-10-06 01:08:16 +0300

    Fixed minor algorithm issues, added input system

--- source/main.cpp
+++ source/main.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
-#include "cache.hpp"
+#include "../include/cache.hpp"
 
 
 int main(int argc, char* argv[]) {
 
     test_pages();
     test_mem();
-    test_cache();
+    // test_cache();
 
-    int buffer_size = 10;
-
-    cache_t<int> two_q_cache(buffer_size); 
-
-    tests_t<int> tests(50);
-    
-    tests.test_cache(two_q_cache);
+    acquire_input();
 
 
     return 0;
